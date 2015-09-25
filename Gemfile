fef0acedcd6a35aeafe0cb570f02fd4504a4ad11
Rich Daley <rich@owl.me.uk>
2015-09-25 07:37:53 +0100

    Some more spec for the DB stuff

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'twitter', '~> 5.15'
+gem 'sqlite3', '~> 1.3'
 
 group :test do
   gem 'cucumber'
--- spec/tkannounce_spec.rb
+++ spec/tkannounce_spec.rb
@@ -7,8 +7,21 @@ RSpec.describe TkAnnounce do
   it { expect(subject.twitter).to be_a(Twitter::REST::Client) }
   it { expect(subject.vendors).to be_an(Array) }
 
-  context '#add_vendor' do
-    it { expect { subject.add_vendor build(:vendor)}.to change { subject.vendors.length }.by(1) }
+  describe '#add_vendor' do
+    it 'adds 1 item to vendors' do
+      v = build(:vendor)
+      expect { subject.add_vendor v }.to change { subject.vendors.length }.by(1)
+    end
+  end
+
+  describe '#tweet_new_vendors!' do
+    it 'tweets only those vendors that are not in the DB'
+    it 'updates the DB to contain those vendors'
+  end
+
+  describe '#vendors_from_db' do
+    it 'contains vendors that are already in the DB'
+    it %{doesn't contain vendors that have been added since}
   end
 
 end
